9661043c293b0485daa6bcdd08e8760cde07519f
ErxAr115 <ernesto_armenta_2000@hotmail.com>
2023-10-15 19:18:44 -0700

    webhook

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
 from routes.site_routes import site
+from routes.chatbot_routes import chatbot
 app = Flask(__name__)
 app.register_blueprint(site)
+app.register_blueprint(chatbot)
